56976b8df2744b7003c1bd76352c28db039bfd92
Jonathon Baxendell <j.bax626@gmail.com>
2022-11-09 20:04:56 -0600

    base

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+Route::get("/todo/{id}", [TodoController::class, 'show']);
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get("/todo", [TodoController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
